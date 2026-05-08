2334e2cb1f076dbc968ab01b1044d30d88469533
Jade <huxenreehan@gmail.com>
2026-05-08 21:47:41 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Handle connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // When a message is received, send it to everyone
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Chat server running at http://localhost:${PORT}`);
+});
